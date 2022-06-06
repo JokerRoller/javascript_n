28728ece87a3322cd96ce2def73c20fea2d97262
JokerRoller <BECOOL686868@gmail.com>
2022-06-06 21:42:54 +0400

    asd

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-// რევერსი, პირველი ვარიანტი
+// // რევერსი, პირველი ვარიანტი
 
-var fruit1 = ['ვაშლი', 'მსხალი', 'ატამი'];
+// var fruit1 = ['ვაშლი', 'მსხალი', 'ატამი'];
 
-document.write(fruit1.reverse() + '</br>');
+// document.write(fruit1.reverse() + '</br>');
 
-// რევერსი, მეორე ვარიანტი
+// // რევერსი, მეორე ვარიანტი
 
-var fruit1 = ['ვაშლი', 'მსხალი', 'ატამი'];
+// var fruit1 = ['ვაშლი', 'მსხალი', 'ატამი'];
 
-for (var i = fruit1.length - 1; i > -1; i-- ) {
+// for (var i = fruit1.length - 1; i > -1; i-- ) {
 
-    document.write(fruit1[i] + ',');
-}
+//     document.write(fruit1[i] + ',');
+// }
 
+function User(pName, pAge) {
+    this.name = pName;
+    this.age = pAge;
+    this.displayInfo = function(){
+    document.write("სახელი: " + this.name + "; ასაკი: " + this.age + "<br/>");
+    };
+    };
+    User.prototype.hello = function(){
+    document.write(this.name + " ამბობს: 'გამარჯობა!'<br/>");
+    };
+    User.prototype.maxAge = 110;
+    var tom = new User("ტომი", 26);
+    tom.hello();
+    var john = new User("ჯონი", 28);
+    john.hello();
+    console.log(tom.maxAge); // 110
