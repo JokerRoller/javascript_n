bdd32932c6593e327c7a71fcf21d08a3224ef1a7
JokerRoller <BECOOL686868@gmail.com>
2022-06-06 22:16:19 +0400

    ასდ

--- index.js
+++ index.js
@@ -1,31 +1,51 @@
-// // რევერსი, პირველი ვარიანტი
+// რევერსი, პირველი ვარიანტი
 
-// var fruit1 = ['ვაშლი', 'მსხალი', 'ატამი'];
+var fruit1 = ['ვაშლი', 'მსხალი', 'ატამი'];
 
-// document.write(fruit1.reverse() + '</br>');
+document.write(fruit1.reverse() + '</br>');
 
-// // რევერსი, მეორე ვარიანტი
+// რევერსი, მეორე ვარიანტი
 
-// var fruit1 = ['ვაშლი', 'მსხალი', 'ატამი'];
+var fruit1 = ['ვაშლი', 'მსხალი', 'ატამი'];
 
-// for (var i = fruit1.length - 1; i > -1; i-- ) {
+for (var i = fruit1.length - 1; i > -1; i-- ) {
 
-//     document.write(fruit1[i] + ',');
-// }
+    document.write(fruit1[i] + ',');
+}
 
-function User(pName, pAge) {
-    this.name = pName;
-    this.age = pAge;
-    this.displayInfo = function(){
-    document.write("სახელი: " + this.name + "; ასაკი: " + this.age + "<br/>");
-    };
-    };
-    User.prototype.hello = function(){
-    document.write(this.name + " ამბობს: 'გამარჯობა!'<br/>");
-    };
-    User.prototype.maxAge = 110;
-    var tom = new User("ტომი", 26);
-    tom.hello();
-    var john = new User("ჯონი", 28);
-    john.hello();
-    console.log(tom.maxAge); // 110
+document.write('</br>'); // კოდის სილამაზისთვის
+
+// სეზონის კალკულატორი
+
+let respond = 'პირველი კოდი არ მუშაობს!'
+
+let months = ['იანვარი', 'თებერვალი', 'მარტი', 'აპრილი', 'მაისი', 'ივნისი', 'ივლისი', 'აგვისტო', 'სექტემბერი', 'ოქტომბერი', 'ნოემბერი', 'დეკემბერი'];
+
+let currentdate = new Date();
+
+if ( currentdate.getMonth() >= 0 && currentdate.getMonth() <= 3 ) {
+    respond = 'ძალიან ცივა';
+}
+else if ( currentdate.getMonth() >= 4 && currentdate.getMonth() <= 8 ) {
+    respond = 'ცხავს';
+}
+else {
+    respond = 'ცივა';
+}
+
+document.write(respond + '</br>');
+
+let respond2 = 'მეორე კოდი არ მუშაობს!'
+
+if ( respond === 'ძალიან ცივა' && currentdate.getDay() >= 4 && currentdate.getDay() <= 6 ) {
+    respond2 = 'საგუნდაო ამინდია გუნდა კეთდება!';
+}
+else if ( respond === 'ცხავს' && currentdate.getDay() >= 4 && currentdate.getDay() <= 6 ) {
+    respond2 = 'Summer Time!';
+}
+else {
+    respond2 = 'წავედით ლექციაზე!'
+}
+
+document.write(respond2);
+
